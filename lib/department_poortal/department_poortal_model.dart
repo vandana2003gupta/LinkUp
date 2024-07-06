77af7549ae418ee24527148da5453c0f4085230c
Utkarsh--1541027 <utkarshsharma1306@gmail.com>
2024-07-06 15:52:28 +0530

    Create department_poortal_model.dart

---
+++ lib/department_poortal/department_poortal_model.dart
@@ -0,0 +1,23 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'department_poortal_widget.dart' show DepartmentPoortalWidget;
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class DepartmentPoortalModel extends FlutterFlowModel<DepartmentPoortalWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+  }
+}
