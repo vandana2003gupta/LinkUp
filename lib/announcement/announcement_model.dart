de95a735b64c0069dbf19bd6936ac838a60b260c
Utkarsh--1541027 <utkarshsharma1306@gmail.com>
2024-07-06 15:51:09 +0530

    Create announcement_model.dart

---
+++ lib/announcement/announcement_model.dart
@@ -0,0 +1,28 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'announcement_widget.dart' show AnnouncementWidget;
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class AnnouncementModel extends FlutterFlowModel<AnnouncementWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressTextController;
+  String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    emailAddressFocusNode?.dispose();
+    emailAddressTextController?.dispose();
+  }
+}
