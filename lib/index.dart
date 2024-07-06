086113548542189d338b22fc216d8354b33fc2d2
Utkarsh--1541027 <utkarshsharma1306@gmail.com>
2024-07-06 15:55:47 +0530

    Create index.dart

---
+++ lib/index.dart
@@ -0,0 +1,14 @@
+// Export pages
+export '/landing_page/landing_page_widget.dart' show LandingPageWidget;
+export '/editproflie/editproflie_widget.dart' show EditproflieWidget;
+export '/signinpage/signinpage_widget.dart' show SigninpageWidget;
+export '/announcement/announcement_widget.dart' show AnnouncementWidget;
+export '/search/search_widget.dart' show SearchWidget;
+export '/portal/portal_widget.dart' show PortalWidget;
+export '/department_poortal/department_poortal_widget.dart'
+    show DepartmentPoortalWidget;
+export '/students/students_widget.dart' show StudentsWidget;
+export '/professor/professor_widget.dart' show ProfessorWidget;
+export '/documents/documents_widget.dart' show DocumentsWidget;
+export '/home/home_widget.dart' show HomeWidget;
+export '/profile/profile_widget.dart' show ProfileWidget;
